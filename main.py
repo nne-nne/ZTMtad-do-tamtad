5aae5c3b5128b90605d37897443ad1de8af29576
Adam Leczkowski <58361333+nne-nne@users.noreply.github.com>
2023-01-12 12:15:16 +0100

    stops request

--- main.py
+++ main.py
@@ -9,6 +9,6 @@ def print_hi(name):
 if __name__ == '__main__':
     print_hi('PyCharm')
     webService = webservice.Webservice()
-    webService.exampleRequest()
+    print(webService.stopsRequest())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
--- webservice.py
+++ webservice.py
@@ -8,3 +8,7 @@ class Webservice:
     def exampleRequest(self):
         x = requests.get(url="https://ckan2.multimediagdansk.pl/shapes?date=2023-01-15&routeId=10606&tripId=371")
         print(x.text)
+
+    def stopsRequest(self) -> str:
+        response = requests.get(url="https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4c4025f0-01bf-41f7-a39f-d156d201b82b/download/stops.json")
+        return response.text
