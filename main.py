e9deb7b3d0981556d3b3e0c568dcd0856b1105db
Adam Leczkowski <58361333+nne-nne@users.noreply.github.com>
2023-01-12 13:38:57 +0100

    dataLoader to user input

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import webservice
 import user_input
+from DataLoader import DataLoader
 from pprint import pprint
 
+data_loader = DataLoader()
 webService = webservice.Webservice()
-user_input = user_input.UserInput()
+user_input = user_input.UserInput(data_loader)
+
 
 if __name__ == '__main__':
+    pass
--- user_input.py
+++ user_input.py
@@ -1,8 +1,13 @@
+from DataLoader import DataLoader
+
+
 class UserInput:
-    def __init__(self):
-        pass
+    def __init__(self, data_loader: DataLoader):
+        self.dataLoader = data_loader
 
     def areStopsConnectedDialogue(self):
         print("provide stop id")
         stopId = input()
         print(stopId)
+
+
