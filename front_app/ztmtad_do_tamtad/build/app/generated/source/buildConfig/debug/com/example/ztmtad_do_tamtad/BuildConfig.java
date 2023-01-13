160e4d5e686cc5f88dae6fef5d59ad5f489743f6
Adam Leczkowski <58361333+nne-nne@users.noreply.github.com>
2023-01-13 08:40:57 +0100

    build

---
+++ front_app/ztmtad_do_tamtad/build/app/generated/source/buildConfig/debug/com/example/ztmtad_do_tamtad/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.ztmtad_do_tamtad;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.ztmtad_do_tamtad";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+}
