20e31978490a10398b5f803fcadc50e298477d13
Jakub Stromski <stromski.jakub.jan@gmail.com>
2023-01-13 06:31:49 +0100

    Downloading ZTM info

--- DataLoader.py
+++ DataLoader.py
@@ -3,6 +3,14 @@ from pprint import pprint
 from tqdm import tqdm
 from datetime import datetime
 
+from webservice import Webservice
+
+route_url = "https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/22313c56-5acf-41c7-a5fd-dc5dc72b3851/download/routes.json"
+departures_url = "https://ckan2.multimediagdansk.pl/departures"
+stops_url = "https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/4c4025f0-01bf-41f7-a39f-d156d201b82b/download/stops.json"
+trips_url = "https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/b15bb11c-7e06-4685-964e-3db7775f912f/download/trips.json"
+stopsintrip_url = "https://ckan.multimediagdansk.pl/dataset/c24aa637-3619-4dc2-a171-a23eec8f2172/resource/3115d29d-b763-4af5-93f6-763b835967d6/download/stopsintrip.json"
+
 
 class DataLoader:
     def __init__(self):
@@ -21,9 +29,19 @@ class DataLoader:
         self.date = datetime.today().strftime('%Y-%m-%d')
 
     def full_prepare(self):
-        self.load_from_files().create_stop_dict().create_route_trip()
+        #self.load_from_files()
+        self.download_data()
+        self.create_stop_dict().create_route_trip()
         self.create_trips_stops().create_stops_connections()
         return self
+
+    def download_data(self):
+        webservice = Webservice()
+        self.routes = webservice.getJson(route_url)
+        self.departures = webservice.getJson(departures_url)
+        self.stops = webservice.getJson(stops_url)
+        self.trips = webservice.getJson(trips_url)
+        self.stopsintrip = webservice.getJson(stopsintrip_url)
 
     def load_from_files(self) -> object:
         with open('Data/routes.json', 'r', encoding='utf8') as f:
