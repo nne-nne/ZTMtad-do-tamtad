8982074d8d5103eba2315dab4bd4a0ea0ea5ea0d
Adam Leczkowski <58361333+nne-nne@users.noreply.github.com>
2023-01-13 05:22:05 +0100

    request for path

--- api.py
+++ api.py
@@ -7,7 +7,29 @@ import path_finder
 from DataLoader import DataLoader
 from pprint import pprint
 
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
+
+
+class Item(BaseModel):
+    stop1_str: str
+    stop2_str: str
+
+
 app = FastAPI()
+
+origins = [
+    "*",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 data_loader = DataLoader().full_prepare()
 webService = webservice.Webservice()
 path_finder = path_finder.PathFinder(data_loader)
@@ -16,6 +38,7 @@ user_input = user_input.UserInput(data_loader, path_finder)
 
 @app.get("/")
 def read_root():
+    print("home request")
     return {"Hello": "World"}
 
 
@@ -27,3 +50,11 @@ async def read_item(item_id: int, q: Union[str, None] = None):
 @app.get("/departure/{stop1_id}/{stop2_id}")
 async def read_item(stop1_id: int, stop2_id: int):
     return path_finder.first_departure_from_stop_ids(stop1_id, stop2_id)
+
+
+@app.post("/paths/")
+async def create_item(item: Item):
+    return path_finder.stopnames_user_request_handler(item.stop1_str, item.stop2_str)
+
+
+
--- path_finder.py
+++ path_finder.py
@@ -1,4 +1,7 @@
+import json
+
 from DataLoader import DataLoader
+from path import Path, PathSegment
 
 
 class PathFinder:
@@ -20,3 +23,21 @@ class PathFinder:
             return
         best_departure = PathFinder.first_departure_from_ways(departures, connections)
         return best_departure
+
+    def stopnames_user_request_handler(self, stop1_str, stop2_str):
+        # dostajemy od użytkownika dwa parametry stopDesc przystanków
+        # czyli na przykład "Gdańsk Wrzeszcz", "Brama Wyżynna"
+        # trzeba wywołać magiczny algorytm dla wszystkich stopId, które pasują
+        # i wysłać w wyniku kilka najlepszych tras (Path)
+
+        print(stop1_str, ' -> ', stop2_str)
+
+        # poniżej przykład:
+        ps1 = PathSegment("6", "TRAM", 13, "2023-01-13T06:30", "Miszewskiego")
+        ps2 = PathSegment("5", "TRAM", 7, "2023-01-13T06:45", "Jaśkowa Dolina")
+        ps3 = PathSegment("154", "BUS", 22, "2023-01-13T06:59", "Klonowa")
+        p1 = Path(58, [ps1, ps2, ps3], "2023-01-13T07:25")
+        p2 = Path(23, [ps1, ps2], "2023-01-13T07:01")
+        result = [p1, p2]
+
+        return result
